2825e4a638199cd4bae7cc409fbba69e8e470b6c
Exodin1 <exodin1@outlook.com>
2022-01-14 17:52:17 -0300

    Requisito 4

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const bodyParser = require('body-parser');
 const fs = require('fs').promises;
 const verifyEmail = require('./middlewares/verifyEmail');
 const verifyPassword = require('./middlewares/verifyPassword');
+const { verifyAge,
+        verifyWatchedAt,
+        verifyToken,
+        verifyTalk,
+        verifyRate,
+        verifyName } = require('./middlewares/verifyFunc');
 
 const talker = './talker.json';
 
@@ -10,7 +16,6 @@ const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
-const HTTP_OK = 200;
 const HTTP_ERROR = 404;
 const PORT = '3000';
 
@@ -26,8 +31,8 @@ app.get('/', (_request, response) => {
 
 app.get('/talker', async (_req, res) => {
   const data = await jsonParseFunc();
-  if (data.length === 0) return res.status(HTTP_OK).json([]);
-  return res.status(HTTP_OK).json(data);
+  if (data.length === 0) return res.status(HTTP_OK_STATUS).json([]);
+  return res.status(HTTP_OK_STATUS).json(data);
 });
 
 app.get('/talker/:id', async (req, res) => {
@@ -37,11 +42,26 @@ app.get('/talker/:id', async (req, res) => {
   if (!talkerFind) {
     return res.status(HTTP_ERROR).json({ message: 'Pessoa palestrante não encontrada' });
   }
-  return res.status(HTTP_OK).json(talkerFind);
+  return res.status(HTTP_OK_STATUS).json(talkerFind);
 });
 
 app.post('/login', verifyEmail, verifyPassword, (_req, res) => {
-  res.status(HTTP_OK).json({ token: '7mqaVRXJSp886CGr' });
+  res.status(HTTP_OK_STATUS).json({ token: '7mqaVRXJSp886CGr' });
+});
+
+app.post('/talker',
+ verifyToken,
+ verifyName,
+ verifyAge,
+ verifyTalk,
+ verifyRate,
+ verifyWatchedAt,
+  async (req, res) => {
+    const data = await jsonParseFunc();
+    const newTalker = { id: data.length + 1, ...req.body };
+    data.push(newTalker);
+    await fs.writeFile(talker, JSON.stringify(data));
+    return res.status(201).json(newTalker);
 });
 
 app.listen(PORT, () => {
